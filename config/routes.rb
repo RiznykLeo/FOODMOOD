ccff2d590d855b13a80319aef136d66764bae83c
Daniel Domingo <domingo.daniel21@gmail.com>
2022-08-23 12:04:23 +0900

    almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :recipes, only: [:index, :show] do
+    resources :user_recipes, only: [:create]
+  end
+  resources :user_recipes, only: [:update]
+
+  resources :shopping_list, only: [:show]
 end

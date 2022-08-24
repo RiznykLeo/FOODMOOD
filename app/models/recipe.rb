223790f848b8e375a2aa001dea3d70d7c210c0d8
Jan Roth <jan-gabriel.roth@gmx.de>
2022-08-24 13:25:44 +0900

    added shopping list controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   has_one_attached :photo
   has_many :ingredients
   has_many :foods, through: :ingredients
+  has_many :shopping_recipes
+  has_many :shopping_lists, through: :shopping_recipes
 end

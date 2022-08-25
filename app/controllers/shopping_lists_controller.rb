fbfbb62158e581c1611f4cf29b0c9523cf2f297e
Jan Roth <jan-gabriel.roth@gmx.de>
2022-08-25 17:02:02 +0900

    added archived button to the shopping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,4 +3,20 @@ class ShoppingListsController < ApplicationController
     @list = current_user.current_shopping_list
     authorize @list
   end
+
+  def update
+    @shopping_list = ShoppingList.find(params[:id])
+    authorize @shopping_list
+    if @shopping_list.update(shopping_list_params)
+      redirect_to "/list"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def shopping_list_params
+    params.require(:shopping_list).permit(:completed)
+  end
 end

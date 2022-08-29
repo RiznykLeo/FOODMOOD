2909817deb40cf08c894bd30d63bd52e8a083496
Daniel Domingo <domingo.daniel21@gmail.com>
2022-08-29 15:24:59 +0900

    unshuffled recipe cards

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
 
   def index
     @user_recipe = UserRecipe.new
-    @recipes = policy_scope(Recipe).where.not(id: current_user.recipes).shuffle
+    @recipes = policy_scope(Recipe).where.not(id: current_user.recipes)
     if params[:recipe]
       top_recipe = @recipes.find{|recipe| recipe.id == params[:recipe].to_i}
       @recipes.delete_at(@recipes.index(top_recipe))

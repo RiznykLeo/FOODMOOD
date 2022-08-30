229210af6f19d1d7f303eebad3286dffc4590348
leonid riznyk <riznyk.leo@gmail.com>
2022-08-30 17:51:37 +0900

    done

--- app/javascript/controllers/swinger_controller.js
+++ app/javascript/controllers/swinger_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
     connect() {
         this.cards = [].slice.call(this.cardTargets)
 
-        const throwOutThreshold = 1;
+        const throwOutThreshold = 0.1;
         const config = {
             isThrowOut: function (xOffset, yOffset, element, throwOutConfidence) {
                 return throwOutConfidence >= throwOutThreshold;
